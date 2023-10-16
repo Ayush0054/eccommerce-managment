d5cfc73cbeb599cd6c6ed35f9c20635191f4ba20
Ayush0054 <ayush5aprill@gmail.com>
2023-10-17 02:00:27 +0530

    signup

--- client/src/pages/signup.tsx
+++ client/src/pages/signup.tsx
@@ -1,9 +1,33 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import React from "react";
 import image2 from "../assets/photo/signup-image.jpeg";
+
+import { signup } from "@/api/auth";
 function Signup() {
+  const [signupData, setSignupData] = useState<SignupParams>({
+    name: "",
+
+    Email: "",
+    password: "",
+  });
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const data = await signup(signupData);
+    //@ts-ignore
+    if (data?.status === "fail") {
+      alert("Something went wrong. Can't signup.");
+      return;
+    }
+
+    if (!data) return;
+
+    // dispatch(userSignupAction({ token: data.token, user: data.user }));
+  };
+
   return (
     <div className=" flex items-center  m-10 mt-36 justify-center ">
       <Card className="flex items-center gap-5 p-10 bg-emerald-200">
